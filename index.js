418d35f73433d725e5fd7908e7b7b3acfbafafbe
Саша Васильчук <sanyabatyushka@gmail.com>
2021-11-20 03:47:40 +0200

    modal

--- index.js
+++ index.js
@@ -28,3 +28,19 @@ function showSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 }
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
